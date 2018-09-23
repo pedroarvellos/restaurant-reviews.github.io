3a92fb53c40c077be668cb47e17befb6e550494e
Pedro Arvellos <pedroarvellos@hotmail.com>
2018-09-23 14:40:30 -0300

    fix: Including changes in accessibility

--- js/main.js
+++ js/main.js
@@ -156,9 +156,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute("alt", "Restaurant image");
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
